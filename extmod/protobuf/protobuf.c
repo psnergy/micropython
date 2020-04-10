17ed56c3e5cce5fcf49ca1c9928dbad0036c79b4
Aditya Naik <naik.aditya23@gmail.com>
2020-04-10 04:22:45 -0400

    Working get_msg_field_id

--- extmod/protobuf/protobuf.c
+++ extmod/protobuf/protobuf.c
@@ -17,11 +17,29 @@
 #include "handshake.pb.h"
 #include "pb.h"
 
-
 #define WRITE_VALUE(X, structname, fieldname, value) X(structname, fieldname, value)
-s2m_MDR_response response = s2m_MDR_response_init_default;
-void* fieldlist_arr[5] = {&response.module_id};
-STATIC mp_map_elem_t *dict_iter_nxt(mp_obj_dict_t *dict, size_t *cur);
+
+STATIC mp_map_elem_t *dict_iter_next(mp_obj_dict_t *dict, size_t *cur);
+STATIC mp_obj_t protobuf_encode(mp_obj_t obj, mp_obj_t stream, mp_obj_t msg_str);
+pb_ostream_t pb_ostream_from_mp_stream(mp_obj_t stream);
+static bool write_callback(pb_ostream_t *stream, const uint8_t *buf, size_t count);
+int get_msg_id(mp_obj_t msg);
+int get_msg_field_id(int msg_id, mp_obj_t msg_field);
+
+
+STATIC mp_obj_t pb_enc(mp_obj_t obj, mp_obj_t stream, mp_obj_t msg_str) {
+    int msg_id = get_msg_id(msg_str);
+    
+    mp_obj_dict_t *self = MP_OBJ_TO_PTR(obj);
+    mp_map_elem_t *elem = NULL;
+    size_t cur = 0;
+    
+    while ((elem = dict_iter_next(self, &cur)) != NULL) {
+	int msg_field_id = get_msg_field_id(msg_id, elem->key);	
+    }
+
+    return mp_obj_new_int(msg_id);
+}
 
 static bool write_callback(pb_ostream_t *stream, const uint8_t *buf, size_t count) {
     int errcode;    
@@ -37,15 +55,112 @@ pb_ostream_t pb_ostream_from_mp_stream(mp_obj_t stream) {
     return pb_stream;
 }
 
-STATIC mp_obj_t protobuf_encode(mp_obj_t obj, mp_obj_t stream, mp_obj_t msg_str) {    
+int get_msg_id(mp_obj_t msg)
+{    
+    const char m2sMDR_req[] = "m2s_MDR_request",
+	s2mMDR_req_ACK[] = "s2m_MDR_req_ACK",
+	m2sMDR_res_CTS[] = "m2s_MDR_res_CTS",
+	s2mMDR_response[] = "s2m_MDR_response";
+    
+    int id = 0; /* Default case is no matching ID */
+    
+    const char *msg_buf;
+    msg_buf = mp_obj_str_get_str(msg);
+
+    if (strcmp(msg_buf, m2sMDR_req) == 0)
+	id = 1;
+    else if (strcmp(msg_buf, s2mMDR_req_ACK) == 0)
+	id = 2;
+    else if (strcmp(msg_buf, m2sMDR_res_CTS) == 0)
+	id = 3;
+    else if (strcmp(msg_buf, s2mMDR_response) == 0)
+	id = 4;
+    return id;
+}
+
+int get_msg_field_id(int msg_id, mp_obj_t msg_field)
+{
+    const char msg1_record_type[] = "record_type",
+	msg2_MDR_res_length[] = "MDR_res_length",
+	msg3_timeout[] = "timeout",
+	msg4_MDR_version[] = "MDR_version",
+	msg4_module_id[] = "module_id",
+	msg4_module_class[] = "module_class",
+	msg4_entity_id[] = "entity_id",
+	msg4_subscriptions[] = "subscriptions",
+	msg_sub_i2c_address[] = "i2c_address";
+
+    int id = 0;
+
+    const char *msg_buf;
+    msg_buf = mp_obj_str_get_str(msg_field);
+    switch (msg_id) {
+    case 2:
+    {
+	if (strcmp(msg2_MDR_res_length, msg_buf) == 0)
+	    id = 1;
+	break;
+    }
+    case 4:
+	if (strcmp(msg4_MDR_version, msg_buf) == 0)
+	    id = 1;
+	else if (strcmp(msg4_module_id, msg_buf) == 0)
+	    id = 2;
+	else if (strcmp(msg4_module_class, msg_buf) == 0)
+	    id = 3;
+	else if (strcmp(msg4_entity_id, msg_buf) == 0)
+	    id = 4;
+	else if (strcmp(msg4_subscriptions, msg_buf) == 0)
+	    id = 5;
+	break;
+    }
+    
+    return id;
+}
+
+STATIC mp_map_elem_t *dict_iter_next(mp_obj_dict_t *dict, size_t *cur) {
+    size_t max = dict->map.alloc;
+    mp_map_t *map = &dict->map;
+
+    for (size_t i = *cur; i < max; i++) {
+        if (mp_map_slot_is_filled(map, i)) {
+            *cur = i + 1;
+            return &(map->table[i]);
+        }
+    }
+
+    return NULL;
+}
+
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(protobuf_encode_obj, protobuf_encode);
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(pb_enc_obj, pb_enc);
+
+STATIC const mp_rom_map_elem_t protobuf_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_protobuf) },
+    { MP_ROM_QSTR(MP_QSTR_encode), MP_ROM_PTR(&protobuf_encode_obj) },
+    { MP_ROM_QSTR(MP_QSTR_pb_enc), MP_ROM_PTR(&pb_enc_obj) },
+};
+
+
+
+STATIC MP_DEFINE_CONST_DICT(protobuf_module_globals, protobuf_globals_table);
+const mp_obj_module_t protobuf_user_cmodule = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t*)&protobuf_module_globals,
+};
+MP_REGISTER_MODULE(MP_QSTR_protobuf, protobuf_user_cmodule, MODULE_EXAMPLE_ENABLED);
+
+STATIC mp_obj_t protobuf_encode(mp_obj_t obj, mp_obj_t stream, mp_obj_t msg_str)
+{
     /* HOW TO ACTUALLY READ A DICTIONARY, WORK IN PROGRESS */    
     mp_obj_dict_t *self = MP_OBJ_TO_PTR(obj);
     mp_map_elem_t *elem = NULL;
     size_t cur = 0;
-    elem = dict_iter_nxt(self, &cur);
+    elem = dict_iter_next(self, &cur);
 
     char test[] = "MDR_res_length";    
-    char *msg_buf;
+    const char *msg_buf;
     msg_buf = mp_obj_str_get_str(elem->key);
     printf("elem key:%s\n", msg_buf);
     
@@ -105,64 +220,3 @@ STATIC mp_obj_t protobuf_encode(mp_obj_t obj, mp_obj_t stream, mp_obj_t msg_str)
     /* pb_ostream_t pb_stream = pb_ostream_from_buffer(buffer, sizeof(buffer)); */
     
 }
-
-int get_msg_id(mp_obj_t msg)
-{    
-    const char m2sMDR_req[] = "m2s_MDR_request",
-	s2mMDR_req_ACK[] = "s2m_MDR_req_ACK",
-	m2sMDR_res_CTS[] = "m2s_MDR_res_CTS",
-	s2mMDR_response[] = "s2m_MDR_response";
-    
-    int id = 0; /* Default case is no matching ID */
-    
-    const char *msg_buf;
-    msg_buf = mp_obj_str_get_str(msg);
-
-    if (strcmp(msg_buf, m2sMDR_req) == 0)
-	id = 1;
-    else if (strcmp(msg_buf, s2mMDR_req_ACK) == 0)
-	id = 2;
-    else if (strcmp(msg_buf, m2sMDR_res_CTS) == 0)
-	id = 3;
-    else if (strcmp(msg_buf, s2mMDR_response) == 0)
-	id = 4;
-    return id;
-}
-
-STATIC mp_obj_t pb_enc(mp_obj_t obj, mp_obj_t stream, mp_obj_t msg_str) {
-    int msg_id = get_msg_id(msg_str);   
-    return mp_obj_new_int(msg_id);
-}
-
-STATIC mp_map_elem_t *dict_iter_nxt(mp_obj_dict_t *dict, size_t *cur) {
-    size_t max = dict->map.alloc;
-    mp_map_t *map = &dict->map;
-
-    for (size_t i = *cur; i < max; i++) {
-        if (mp_map_slot_is_filled(map, i)) {
-            *cur = i + 1;
-            return &(map->table[i]);
-        }
-    }
-
-    return NULL;
-}
-
-
-STATIC MP_DEFINE_CONST_FUN_OBJ_3(protobuf_encode_obj, protobuf_encode);
-STATIC MP_DEFINE_CONST_FUN_OBJ_3(pb_enc_obj, pb_enc);
-
-STATIC const mp_rom_map_elem_t protobuf_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_protobuf) },
-    { MP_ROM_QSTR(MP_QSTR_encode), MP_ROM_PTR(&protobuf_encode_obj) },
-    { MP_ROM_QSTR(MP_QSTR_pb_enc), MP_ROM_PTR(&pb_enc_obj) },
-};
-
-
-
-STATIC MP_DEFINE_CONST_DICT(protobuf_module_globals, protobuf_globals_table);
-const mp_obj_module_t protobuf_user_cmodule = {
-    .base = { &mp_type_module },
-    .globals = (mp_obj_dict_t*)&protobuf_module_globals,
-};
-MP_REGISTER_MODULE(MP_QSTR_protobuf, protobuf_user_cmodule, MODULE_EXAMPLE_ENABLED);
