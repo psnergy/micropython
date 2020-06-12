0a89fee969cb4db531f9c3cf4c4cd5c05e8bf7ad
Aditya Naik <naik.aditya23@gmail.com>
2020-06-12 15:58:44 -0400

    micropython script changes

--- ports/unix/test.py
+++ ports/unix/test.py
@@ -17,44 +17,35 @@ import uio, protobuf
 """
 I2C device configuration and initialization. This should be part of the superloop file.
 """
-SLAVE_ADDRESS = 0x2
+SLAVE_ADDRESS = 0x4 # actual bus address: 0x8
 BAUDRATE = 100000
 i2c_slave = I2C(1, I2C.SLAVE, addr=SLAVE_ADDRESS, baudrate=BAUDRATE)
 
 """
 Global configuration options (MDR) for this module. These get encoded and sent to the master during handshake.
 """
-MODULE_VERSION   = 0.1
+MODULE_VERSION   = 0.2
 MODULE_ID        = 2
 MODULE_CLASS     = 3
 ENTITY_ID        = 1
-SUBS_MODULE_IDS  = 0x1F
+SUBS_MODULE_IDS  = 0xFFFF
 SUBS_I2C_ADDRS   = 0
 
 
 def get_i2c_data(i2c_device, recv_size):
-    while True:
-        try:
-            data = i2c_device.recv(recv_size, timeout=100)
-            print("RECV: %r" % data)
-            return data
-        except OSError as exc:
-            if exc.args[0] not in (5, 110):
-                # 5 == EIO, occurs when master does a I2C bus scan
-                # 110 == ETIMEDOUT
-                print("Got nothing")
-        except KeyboardInterrupt:
-            print("Ending")
-            return 0
+    try:
+        data = i2c_device.recv(recv_size, timeout=1000)
+        return data
+    except:
+        return None
             
 def send_i2c_data(i2c_device, data):
-    while True:
-        try:
-            i2c_device.send(data, timeout=100)
-            return True
-        except:
-            continue
-    return False
+    try:
+        i2c_device.send(data, timeout=1000)
+        return True
+    except:
+        return False
+
 
 def handshake(i2c_device):
     """
@@ -70,14 +61,16 @@ def handshake(i2c_device):
     
     encoded_MDR = protobuf.encode(message_fields, msg, stream).getvalue()
     MDR_len = len(encoded_MDR)
-    print("Length: {}, MDR: ".format(MDR_len))
-    print(encoded_MDR)
     
     MDR_ack = bytearray([MDR_len, 0xFF])
         
     data = get_i2c_data(i2c_device, 2)
+    if data is None:
+        return False
     status = send_i2c_data(i2c_device, MDR_ack)
     data = get_i2c_data(i2c_device, 2)
+    if data is None:
+        return False
     status = send_i2c_data(i2c_device, encoded_MDR)
     
     return status
@@ -91,35 +84,72 @@ def send_datapoint(data, i2c_device):
 
     @param data A list of dictionaries which contain fields 
     that are expected by the proto format.
+    @param i2c_device The i2c_device initialized as slave
     """
     stream = uio.BytesIO()
     msg = "s2m_data"
-    encoded_data = protobuf.encode(data, msg, stream).getvalue()    
+    encoded_data = protobuf.encode(data, msg, stream).getvalue()
     enc_data_len = len(encoded_data)
-
-    SOR_buf = get_i2c_data(i2c_device, 4)
-    SOR_stream = uio.BytesIO(SOR_buf)
-    SOR_message=protobuf.decode("m2s_SOR", SOR_stream)
-
-    print(SOR_message)
+    doc_msg = bytearray([0xFF, 5, 0xFF, enc_data_len])
+    SOR_buf = get_i2c_data(i2c_device, 2)
     
-    if SOR_message['SOR_code'] == 1:
-        doc_fields = {'doc_code':5, 'tx_length':enc_data_len}
-        doc_stream = uio.BytesIO()
-        doc_msg=protobuf.encode(doc_fields, 's2m_DOC', doc_stream).getvalue()
-        status = send_i2c_data(i2c_device, doc_msg)
-        
+    if SOR_buf is None:
+        # print("SOR fail")
+        return False
+    print("got sor")
+    if SOR_buf[0] == 1:
+        if send_i2c_data(i2c_device, doc_msg) == False:
+            print("doc send fail")
+            return False
+        print("sent doc")
         data = get_i2c_data(i2c_device, 2)
-        status = send_i2c_data(i2c_device, encoded_data)
-
+        if data is None:
+            print("cts fail")
+            return False
+        print("got cts")
+        if send_i2c_data(i2c_device, encoded_data) == False:
+            print("data send fail")
+            return False
+        print("sent data")
+        return True
+    elif SOR_buf[0] == 2:
+        CTS_buf = bytearray([0x0, 0x1])
+        if send_i2c_data(i2c_device, CTS_buf) == False:
+            print("cts send fail")
+            return False
+        print("got len buf")
+        len_buf = get_i2c_data(i2c_device, 4)
+        MDR_len = len_buf[1]+(len_buf[0]<<8)
+        data_len = len_buf[3]+(len_buf[2]<<8)
+        print("MDR_len: " + str(MDR_len) + " Data len: "+str(data_len))
+        if send_i2c_data(i2c_device, CTS_buf) == False:
+            print("cts send fail")
+            return False
         
+        MDR_buf  = get_i2c_data(i2c_device, MDR_len)
+        data_buf = get_i2c_data(i2c_device, data_len)
+        if MDR_buf is None or data_buf is None:
+            print("failed to get MDR or data")
+            return False
+        print("Got data and MDR")        
+
 """
 Driver code for handshake and dataflow. This should be part of the superloop.
-"""    
-while handshake(i2c_slave) == False:    
-    continue
+"""
+HS_DONE = False
+while not HS_DONE:
+    try:
+        HS_DONE = handshake(i2c_slave)
+    except:
+        continue
 print("Handshake done")
-delay(1000)
-while True:    
-    send_datapoint([{"entity_id":21, "data": 20.70, "unit_id": 2}, {"entity_id":2, "data": 20.70, "unit_id": 1}], i2c_slave)
+# delay(1000)
+while True:
+    try:
+        send_datapoint([{"entity_id":21, "data": 20.70, "unit_id": 2}, {"entity_id":2, "data": 20.70, "unit_id": 1}], i2c_slave)
+    except:
+        continue
+    # delay(100);
+
+
 
